e479dbedde08890e16ee7ed262727f333b88925c
Mohammad yusuf <asdword1200@outlook.com>
2021-05-14 01:53:55 +0430

    remove initail data in context state

--- todo-task/src/context/tasks/TasksState.js
+++ todo-task/src/context/tasks/TasksState.js
@@ -3,18 +3,7 @@ import React, { createContext, useReducer } from 'react';
 import taskReducer from './taskReducer';
 
 const initialState = {
-  tasks: [{
-    id: 1,
-    status: 'low',
-    title: 'some works',
-    decs: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-  },
-  {
-    id: 2,
-    status: 'medium',
-    title: 'other works',
-    decs: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,'
-  }]
+  tasks: []
 }
 
 
